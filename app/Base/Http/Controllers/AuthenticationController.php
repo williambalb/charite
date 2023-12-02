6d8fc7ad843c33394085f5d4186ff21ca968a6a9
William Balbino <williambalbino2014@gmail.com>
2023-12-02 14:59:37 -0300

    [feature/18] Função de cadastro criada

--- app/Base/Http/Controllers/AuthenticationController.php
+++ app/Base/Http/Controllers/AuthenticationController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use App\Base\Http\Requests\RegisterRequest;
 
 class AuthenticationController extends Controller
 {
@@ -38,5 +39,27 @@ class AuthenticationController extends Controller
         } else {
             return response()->json(['status' => 'fail'], 401);
         }
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8'
+        ]);
+        
+        $user = [
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'document' => $request->get('document'),
+            'password' => Hash::make($request->get('password')),
+            'role_id' => 2,
+            'api_key' => base64_encode(Str::random(40))
+        ];
+
+        $this->userRepository->create($user);
+
+        return response()->json(['status' => 'success', 'api_key' => $user['api_key']]);
     }
 }
